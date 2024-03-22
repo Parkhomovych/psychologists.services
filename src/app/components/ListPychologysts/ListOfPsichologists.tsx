ef0c09275ae41bd9cf559ecbb1a0b63be28bf17d
Vladyslav Parkhomovych <adamantinevlad@gmail.com>
2024-03-23 01:20:24 +0200

    fix type Therapist

--- src/app/components/ListPychologysts/ListOfPsichologists.tsx
+++ src/app/components/ListPychologysts/ListOfPsichologists.tsx
@@ -1,9 +1,25 @@
 "use client";
-import { Therapist } from "@/app/psychologists/page";
-import { DocumentData } from "firebase/firestore";
+
 import { usePathname } from "next/navigation";
 import ItemOfPsichologists from "./ItemPsychologysts/ItemOfPsichologists";
 import Filters from "./ItemPsychologysts/Filter/Filters";
+export interface Review {
+  reviewer: string;
+  rating: number;
+  comment: string;
+}
+export interface Therapist {
+  name: string;
+  avatar_url: string;
+  experience: string;
+  reviews: Review[];
+  price_per_hour: number;
+  rating: number;
+  license: string;
+  specialization: string;
+  initial_consultation: string;
+  about: string;
+}
 
 export default function ListOfPsichologists({ data }: any) {
   const pathname = usePathname();
--- src/app/psychologists/page.tsx
+++ src/app/psychologists/page.tsx
@@ -1,23 +1,6 @@
 import { getPshichologists } from "@/firebase/database/getAll";
-import Filters from "../components/ListPychologysts/ItemPsychologysts/Filter/Filters";
 import ListOfPsichologists from "../components/ListPychologysts/ListOfPsichologists";
-export interface Review {
-  reviewer: string;
-  rating: number;
-  comment: string;
-}
-export interface Therapist {
-  name: string;
-  avatar_url: string;
-  experience: string;
-  reviews: Review[];
-  price_per_hour: number;
-  rating: number;
-  license: string;
-  specialization: string;
-  initial_consultation: string;
-  about: string;
-}
+
 export default async function Psyhologists() {
   const data = await getPshichologists();
 
